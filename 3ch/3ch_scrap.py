e2453960f106699c5af031c81dc3d606a47fe200
mccarvik <mccarviks@gmail.com>
2017-07-21 02:02:52 +0000

    small progress

--- 3ch/3ch_scrap.py
+++ 3ch/3ch_scrap.py
@@ -1,10 +1,38 @@
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as minidom
 
 
 def element_tree():
     root = ET.Element('inventory')
-    print(ET.dump(root))
-
+    ET.dump(root)
+    
+    cheese = ET.Element('cheese')
+    root.append(cheese)
+    ET.dump(root)
+    
+    name = ET.SubElement(cheese, 'name')
+    name.text = 'Caerphilly'
+    ET.dump(root)
+    
+    temp = ET.SubElement(root, 'temp')
+    ET.dump(root)
+    root.remove(temp)
+    ET.dump(root)
+    xml_pprint(root)
+    
+    cheese.attrib['id'] = 'c01'
+    xml_pprint(cheese)
+    
+    text = ET.tostring(name)
+    print(text)
+    
+    text = ET.tostring(name, encoding='utf-8')
+    
+    
+    
+def xml_pprint(element):
+    s = ET.tostring(element)
+    print(minidom.parseString(s).toprettyxml())
 
 if __name__ == '__main__':
     element_tree()
