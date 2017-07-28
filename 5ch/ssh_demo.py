bfc786981858fa08fdfdcf85f20a1264ae77cb3a
mccarvik <mccarviks@gmail.com>
2017-07-28 01:36:25 +0000

    adding ch 5, need to find setup tools to convet those egg fiels

---
+++ 5ch/ssh_demo.py
@@ -0,0 +1,20 @@
+import getpass
+import paramiko
+
+HOSTNAME = 'localhost'
+PORT = 22
+
+def run_ssh_cmd(username, password, cmd, hostname=HOSTNAME, port=PORT):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(hostname, port, username, password)
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    print(stdout.read)
+    
+if __name__ == '__main__':
+    username = input("Enter username: ")
+    password = getpass.getpass(prompt='Enter password: ')
+    cmd = 'ls -l /dev'
+    run_ssh_cmd(username, password, cmd)
+    
