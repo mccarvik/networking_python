22548cb680188979fd560d67dbe75fa12e3afa83
mccarvik <mccarviks@gmail.com>
2017-07-30 15:15:58 +0000

    making a bucnh of progress, sockets way more interesting

---
+++ 6ch/dns_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+sys.path.append("/usr/local/lib/python3.4/dist-packages")
+import dns.resolver
+
+if __name__ == '__main__':
+    loookup_continue = True
+    while loookup_continue:
+        name = input('Enter the DNS name to resolve: ')
+        record_type = input('Enter the query type [A/MX/CNAME]: ')
+        answers = dns.resolver.query(name, record_type)
+        #import pdb; pdb.set_trace()
+        if record_type == 'A':
+            print('Got answer IP address: %s' %[x.to_text() for x in answers])
+        elif record_type == 'CNAME':
+            print('Got answer Aliases: %s' %[x.to_text() for x in answers])
+        elif record_type == 'MX':
+            for rdata in answers:
+                print('Got answers for Mail server records:')
+                print('Mailserver', rdata.exchange.to_text(), 'has preference', rdata.preference)
+        else:
+            print('Record type: %s is not implemented' %record_type)
+        lookup_more = input("Do you want to lookup more records? [y/n]: " )
+        if lookup_more.lower() == 'n':
+            loookup_continue = False
+            
